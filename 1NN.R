cb7790c6ef018830a19bdd1fb5175a4ec2fb6aff
jelupre <71133226+jelupre@users.noreply.github.com>
2020-09-18 11:13:11 +0300

    Update 1NN.R

--- 1NN.R
+++ 1NN.R
@@ -11,7 +11,7 @@ oneNN <- function(set, point){
   number_of_nearest <- 1
   
   ## попробуем найти соседа ближе
-  for(i in 2:150){
+  for(i in 2:N){
     if (distance_of_Euclid(set[i, 1:2], point) < min_distance){
       min_distance <- distance_of_Euclid(set[i, 1:2], point)
       number_of_nearest <- i
@@ -26,15 +26,20 @@ oneNN <- function(set, point){
 ## создание тренировочной выборки по ширине и длине лепестка и виду ириса
 train_set <- iris[, 3:5]
 
+## кол-во точек в тренировочной выборке
+N <- dim(train_set)[1]
+
+## кол-во точек в тестовой выборке
+n <- as.numeric(readline(prompt = "Enter number of points in test set: "))
+
 ## создание набора тестовых точек
-points <- cbind(runif(15, 0, 7), runif(15, 0, 2.5))
+points <- cbind(runif(n, 0.1, 6.9), runif(n, 0.1, 2.4))
 
 ## рисуем выборку
 colors <- c("setosa" = "red", "versicolor" = "green4", "virginica" = "blue")
-plot(iris[, 3:4], pch = 21, bg = colors[iris$Species], asp = 1)
+plot(iris[, 1:5], pch = 21, bg = colors[iris$Species], asp = 1)
 
 ## рисуем точки
-for (i in 1:15){
+for (i in 1:n){
   points(points[i, 1], points[i, 2], pch = 24, bg = colors[oneNN(train_set, points[i, 1:2])], asp = 1)
 }
-
